a99c897917f6f4a484c65ca1729f64ec7d3c038e
Benji <67171031+liangbinjie@users.noreply.github.com>
2023-02-01 23:20:20 +0000

    galeria

---
+++ manu/src/main/java/com/manu/manu/ManuApplication.java
@@ -0,0 +1,13 @@
+package com.manu.manu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManuApplication.class, args);
+	}
+
+}
